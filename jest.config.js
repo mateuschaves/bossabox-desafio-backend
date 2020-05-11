9eee75da42f954b9588ea29a65c5ad3fc7d96e27
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-05-10 22:26:40 -0300

    add coverage folder to gitignore

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.spec.ts'],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "__tests__/coverage",
+  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
